e2fb3d0fd4f41a4ff5dd1d7770cd095bce2948c6
Volodymyr Viter <viter.volodymyr.d@gmail.com>
2022-04-08 23:11:35 +0300

    add mat-sort
    - add sorting for table
    - add status checkbox
    - add 'no content' text if no tasks
    - add pagination for tasks (TODO - fix styles when hover, fix dropdown for 'items per page')

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from "../../services/data-handler.service";
 import {Task} from "../../models/Task";
 import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
-  displayedColumns: string[] = ['color', 'id', 'title', 'category', 'priority', 'date'];
+export class TasksComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['color', 'id', 'title', 'category', 'priority', 'date', 'status'];
   dataSource!: MatTableDataSource<Task>
+
+  @ViewChild(MatPaginator, {static: false}) private paginator!: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort!: MatSort;
 
   tasks: Task[] = []
 
@@ -26,6 +31,10 @@ export class TasksComponent implements OnInit {
     this.refreshTable()
   }
 
+  ngAfterViewInit(): void {
+    this.addTableObjects()
+  }
+
   toggleTaskComplete(task: Task) {
     task.completed = !task.completed
   }
@@ -38,10 +47,41 @@ export class TasksComponent implements OnInit {
     if(task.priority && task.priority.color) {
       return task.priority.color
     }
+
     return '#fff'
   }
 
   private refreshTable() {
     this.dataSource.data = this.tasks
+    this.addTableObjects()
+
+    this.dataSource.sortingDataAccessor = (task, colName): any => {
+      switch(colName) {
+        case 'title': {
+          return task.title
+        }
+
+        case 'category': {
+          return task.category ? task.category.title : null
+        }
+
+        case 'priority': {
+          return task.priority ? task.priority.id : null
+        }
+
+        case 'date': {
+          return task.date ? task.date : null
+        }
+
+        case 'status': {
+          return !task.completed
+        }
+      }
+    }
+  }
+
+  private addTableObjects() {
+    this.dataSource.sort = this.sort
+    this.dataSource.paginator = this.paginator
   }
 }
